49d4b316e7f43bad451ba487a4ea25d7870878a6
Francesco <francesco.dambrosio@gmail.com>
2017-10-09 17:45:18 +0200

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+
+import { Media, MediaObject } from '@ionic-native/media';
+import { MusicControls } from '@ionic-native/music-controls';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    Media,
+    MusicControls,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
---
+++ src/pages/home/home.ts
@@ -0,0 +1,111 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { Media, MediaObject } from '@ionic-native/media';
+import { MusicControls } from '@ionic-native/music-controls';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+  file: MediaObject;
+  constructor(private musicControls: MusicControls, private media: Media, public navCtrl: NavController) {
+  }
+  play(){
+    this.file = this.media.create('https://archive.org/download/db2007-05-20.rm.flac16/db2007-05-20d1t01.mp3');
+    this.file.play();
+
+    this.musicControls.destroy(); // it's the same with or without the destroy 
+    this.musicControls.create({
+      track       : 'Test track',        // optional, default : ''
+      artist      : 'test artist',                       // optional, default : ''
+      cover       : '',      // optional, default : nothing
+      // cover can be a local path (use fullpath 'file:///storage/emulated/...', or only 'my_image.jpg' if my_image.jpg is in the www folder of your app)
+      //           or a remote url ('http://...', 'https://...', 'ftp://...')
+      isPlaying   : true,                         // optional, default : true
+      dismissable : true,                         // optional, default : false
+    
+      // hide previous/next/close buttons:
+      hasPrev   : false,      // show previous button, optional, default: true
+      hasNext   : false,      // show next button, optional, default: true
+      hasClose  : true,       // show close button, optional, default: false
+    
+    // iOS only, optional
+      album       : 'test album',     // optional, default: ''
+      duration : 0, // optional, default: 0
+      elapsed : 0, // optional, default: 0
+      hasSkipForward : true,  // show skip forward button, optional, default: false
+      hasSkipBackward : true, // show skip backward button, optional, default: false
+      skipForwardInterval: 15, // display number for skip forward, optional, default: 0
+      skipBackwardInterval: 15, // display number for skip backward, optional, default: 0
+    
+      // Android only, optional
+      // text displayed in the status bar when the notification (and the ticker) are updated
+      ticker    : 'Now playing test'
+     });
+     this.musicControls.subscribe().subscribe(action => {
+         function events(action) {
+          console.log('action', action);
+           const message = JSON.parse(action).message;
+
+               switch(message) {
+                   case 'music-controls-next':
+                       // Do something
+                       break;
+                   case 'music-controls-previous':
+                       // Do something
+                       break;
+                   case 'music-controls-pause':
+                       // Do something
+                       console.log('musc pause');
+                       this.pause();
+                       break;
+                   case 'music-controls-play':
+                       // Do something
+                       console.log('music play');
+                       this.play();
+                       break;
+                   case 'music-controls-destroy':
+                       // Do something
+                       break;
+                  // External controls (iOS only)
+                  case 'music-controls-toggle-play-pause' :
+                    // Do something
+                    break;
+                  case 'music-controls-seek-to':
+                    // Do something
+                    break;
+                  case 'music-controls-skip-forward':
+                    // Do something
+                    break;
+                  case 'music-controls-skip-backward':
+                    // Do something
+                    break;
+          
+                    // Headset events (Android only)
+                    // All media button events are listed below
+                    case 'music-controls-media-button' :
+                        // Do something
+                        break;
+                    case 'music-controls-headset-unplugged':
+                        // Do something
+                        break;
+                    case 'music-controls-headset-plugged':
+                        // Do something
+                        break;
+
+                    default:
+                        break;
+                }
+           }
+      
+       this.musicControls.listen(); // activates the observable above
+       this.musicControls.updateIsPlaying(true);
+
+    });
+}
+
+  pause(){
+    this.file.pause();
+  }
+}
